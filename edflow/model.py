f4fb975e8c3473562a3f6ee43b1eec1cb0564b24
Patrick Esser <Patrick.Esser@gmx.net>
2018-08-30 12:28:10 +0200

    import datasetmixin for convenience, add default variables method to model

--- edflow/model.py
+++ edflow/model.py
@@ -1,7 +1,10 @@
-from edflow.custom_logging import get_logger
+import tensorflow as tf
 
 class Model(object):
     '''Base class defining all neccessary model methods.'''
+    def __init__(self, name):
+        self.model_name = name
+
     @property
     def inputs(self):
         '''Input placeholders'''
@@ -11,3 +14,8 @@ class Model(object):
     def outputs(self):
         '''Output tensors.'''
         raise NotImplementedError()
+
+    @property
+    def variables(self):
+        '''Variables.'''
+        return [v for v in tf.global_variables() if self.model_name in v.name]
