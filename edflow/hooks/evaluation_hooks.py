dc852afebe0ed65fb463afa53ab72fe4c2310d62
Johannes Haux <jo.mobile.2011@gmail.com>
2018-09-21 14:26:37 +0200

    MetricHook now accepts max number of examples argument to speed things up

--- edflow/hooks/evaluation_hooks.py
+++ edflow/hooks/evaluation_hooks.py
@@ -234,7 +234,7 @@ def test_valid_metrictuple(metric_tuple):
 class MetricHook(Hook):
     '''Applies a set of given metrics to the calculated data.'''
 
-    def __init__(self, metrics, save_root):
+    def __init__(self, metrics, save_root, consider_only_first=None):
         '''Args:
             metrics (list): List of ``MetricTuple``s of the form
                 ``(input names, output names, metric, name)``. 
@@ -249,12 +249,16 @@ class MetricHook(Hook):
                 passed as "path" like ``'key1_key2'`` returning
                 ``dict[key1][key2]``.
             save_root (str): Path to where the results are stored.
+            consider_only_first (int): Metric is only evaluated on the first 
+                `consider_only_first` examples.
         '''
 
         self.metrics = metrics
 
         self.root = save_root
         self.logger = get_logger(self, 'latest_eval')
+
+        self.max_step = consider_only_first
 
         self.storage_dict = {}
         self.metric_results = {}
@@ -264,10 +268,14 @@ class MetricHook(Hook):
         self.tb_saver = tf.summary.FileWriter(self.root)
 
     def before_epoch(self, epoch):
+        self.count = 0
         for m in self.metrics:
             self.metric_results[m.name] = []
 
     def before_step(self, step, fetches, feeds, batch):
+        if self.max_step is not None and self.count >= self.max_step:
+            return
+
         for in_names, out_names, metric, m_name in self.metrics:
             self.storage_dict[m_name] = {}
             for kwargs_name, name in in_names.items():
@@ -275,6 +283,9 @@ class MetricHook(Hook):
                 self.storage_dict[m_name][kwargs_name] = val
 
     def after_step(self, step, results):
+        if self.max_step is not None and self.count >= self.max_step:
+            return
+
         for in_names, out_names, metric, m_name in self.metrics:
             for kwargs_name, name in out_names.items():
                 val = self.retrieve(name, results)
@@ -283,16 +294,20 @@ class MetricHook(Hook):
             self.metric_results[m_name] += [m_res]
 
         self.global_step = results['global_step']
+        self.count += 1
 
     def after_epoch(self, epoch):
         self.logger.info("Metrics at epoch {}:".format(epoch))
 
         mean_results = {}
         for name, result in self.metric_results.items():
-            mean = np.mean(np.concatenate(result), axis=0)
-            var = np.std(np.concatenate(result), axis=0)
+            self.logger.info('name: {}'.format(name))
+            self.logger.info('result: {}'.format(result))
+            results = np.concatenate(result)
+            mean = np.mean(results, axis=0)
+            var = np.std(results, axis=0)
             mean_results[name] = np.array([mean, var])
-            self.logger.info("{}: {} {}".format(name, mean, var))
+            self.logger.info("{}: {} +- {}".format(name, mean, var))
 
             summary = tf.Summary()
             summary_mean = mean if len(mean.shape) == 0 else mean[0]
@@ -303,7 +318,7 @@ class MetricHook(Hook):
         name = '{:0>6d}_metrics'.format(self.global_step)
         name = os.path.join(self.root, name)
         np.savez_compressed(name, **mean_results)
-    
+
     def retrieve(self, key, list_or_dict, splitval='/'):
         '''Given a nested list or dict return the desired value at key.'''
 
