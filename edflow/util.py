354cc6d0c159df5ccaf840f48a9c30b77f1fc499
Johannes Haux <johannes.haux@gmx.de>
2018-09-25 11:34:36 +0200

    Added more general PyHookedModelIterator -> Can be used as baseclass for other iterators.

--- edflow/util.py
+++ edflow/util.py
@@ -25,3 +25,38 @@ def make_linear_var(step,
             (end - start) *
             (tf.cast(step, tf.float32) - start) + start_value)
     return tf.clip_by_value(linear, clip_min, clip_max)
+
+
+def walk(dict_or_list, fn):
+    '''Walk a nested list and/or dict recursively and call fn on all non
+    list or dict objects.'''
+
+    def call(value):
+        if isinstance(value, (list, dict)):
+            return walk(value, fn)
+        else:
+            return fn(value)
+
+    if isinstance(dict_or_list, list):
+        results = [call(val) for val in dict_or_list]
+    elif isinstance(dict_or_list, dict):
+        results = {k: call(val) for k, val in dict_or_list.items()}
+    else:
+        results = fn(dict_or_list)
+
+    return results
+
+
+if __name__ == '__main__':
+    nested = {'step': 1,
+              'stuff': {'a': 1, 'b': [1,2,3]},
+              'more': [{'c': 1}, 2, [3, 4]]}
+
+    def fn(val):
+        print(val)
+        return -val
+
+    new = walk(nested, fn)
+
+    print(nested)
+    print(new)
