9b44322727c7a7c4b65f4e8860e798675955371f
Johannes Haux <jo.mobile.2011@gmail.com>
2018-11-20 14:50:01 +0100

    get_impl not dynamically defined anymore, but now reusable

--- edflow/main.py
+++ edflow/main.py
@@ -11,11 +11,13 @@ from edflow.iterators.batches import make_batches
 from edflow.custom_logging import init_project, get_logger, LogSingleton
 
 
+def get_impl(config, name):
+    impl = config[name]
+    module, cls = impl.rsplit(".", 1)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
 def get_implementations_from_config(config, names):
-    def get_impl(config, name):
-        impl = config[name]
-        module, cls = impl.rsplit(".", 1)
-        return getattr(importlib.import_module(module, package=None), cls)
     implementations = dict((name, get_impl(config, name)) for name in names)
     return implementations
 
