28d7c277045a68af796315afc77d09fe63c9a3f8
Patrick Esser <Patrick.Esser@gmx.net>
2019-02-14 11:52:32 +0100

    print training vars, mod frequ trainer, add list trainer

--- edflow/iterators/trainer.py
+++ edflow/iterators/trainer.py
@@ -266,6 +266,11 @@ class TFBaseTrainer(TFHookedModelIterator):
             else:
                 self.optimizers[k] = Optimizer(**opt_kwargs)
             opt_ops[k] = self.optimizers[k].minimize(losses[k], var_list=variables)
+            print(k)
+            print("============================")
+            print("\n".join([v.name for v in variables]))
+            print(len(variables))
+            print("============================")
         self.opt_ops = opt_ops
 
         opt_op = tf.group(*opt_ops.values())
@@ -309,10 +314,78 @@ class TFFrequencyTrainer(TFBaseTrainer):
         self.train_op = None
 
         self.which_opt = list()
-        for k in self.opt_frequency:
-            self.which_opt += self.opt_frequency[k]*[k]
+        if type(self.opt_frequency) == dict:
+            for k in self.opt_frequency:
+                self.which_opt += self.opt_frequency[k]*[k]
+        elif type(self.opt_frequency) == list:
+            self.which_opt = self.opt_frequency
+        else:
+            assert False
 
     def run(self, fetches, feed_dict):
         current_opt = self.which_opt[self.get_global_step() % len(self.which_opt)]
-        fetches["step_ops"] = self.opt_ops[current_opt]
+        if type(current_opt) == list:
+            fetches["step_ops"] = [self.opt_ops[k] for k in current_opt]
+        else:
+            fetches["step_ops"] = self.opt_ops[current_opt]
+        return super().run(fetches, feed_dict)
+
+
+class TFListTrainer(TFBaseTrainer):
+    def create_train_op(self):
+        '''Default optimizer + optimize each submodule'''
+        self.train_placeholders["global_step"] = tf.placeholder(tf.int32, name = "global_step")
+        # workaround of https://github.com/tensorflow/tensorflow/issues/23316
+        self._global_step_variable = tf.Variable(0, dtype = tf.int32, trainable = False)
+        self.global_step = tf.assign(self._global_step_variable, self.train_placeholders["global_step"])
+
+        # Optimizer
+        self.initial_lr = self.config["lr"]
+        self.lr_decay_begin = self.config["lr_decay_begin"]
+        self.lr_decay_end = self.config["lr_decay_end"]
+
+        self.lr = lr = make_linear_var(self.global_step,
+                                       self.lr_decay_begin, self.lr_decay_end,
+                                       self.initial_lr, 0.0,
+                                       0.0, self.initial_lr)
+        optimizer_name = self.config.get("optimizer", "AdamOptimizer")
+        Optimizer = getattr(tf.train, optimizer_name)
+        opt_kwargs = {"learning_rate": lr}
+        if optimizer_name == "AdamOptimizer":
+            opt_kwargs["beta1"] = self.config.get("beta1", 0.5)
+            opt_kwargs["beta2"] = self.config.get("beta2", 0.9)
+
+        self.optimizers = dict()
+        self.all_train_ops = list()
+
+        # Optimization ops
+        list_of_losses = self.make_loss_ops()
+        assert type(list_of_losses) == list
+        for i, losses in enumerate(list_of_losses):
+            opt_ops = dict()
+            optimizers = dict()
+
+            for k in losses:
+                variables = self.get_trainable_variables(k)
+                optimizers[k] = Optimizer(**opt_kwargs)
+                opt_ops[k] = optimizers[k].minimize(losses[k], var_list=variables)
+                print(i, k)
+                print("============================")
+                print("\n".join([v.name for v in variables]))
+                print(len(variables))
+                print("============================")
+
+            opt_op = tf.group(*opt_ops.values())
+            with tf.control_dependencies([opt_op] + self.update_ops):
+                train_op = tf.no_op()
+            self.all_train_ops.append(train_op)
+
+        self.train_op = None # dummy, set actual train op in run
+        # add log ops
+        self.log_ops["global_step"] = self.global_step
+        self.log_ops["lr"] = self.lr
+
+    def run(self, fetches, feed_dict):
+        train_idx = self.get_global_step() % len(self.all_train_ops)
+        fetches["step_ops"] = self.all_train_ops[train_idx]
         return super().run(fetches, feed_dict)
