59160a979a997475f12323d3258e9413ab4426ed
ArWeHei <github@aheimbrecht.de>
2019-04-11 12:47:35 +0200

    added RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -74,6 +74,7 @@ autodoc_mock_imports = ["tensorflow",
         'h5py',
         'scikit-learn',
         'scikit-image',
+        'skimage',
         'natsort'
 ]
 autosummary_generate = True
