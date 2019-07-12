22e04bf01c739edae7b4b801154050550eda5094
Patrick Esser <Patrick.Esser@gmx.net>
2019-07-12 15:27:18 +0200

    formatting

--- edflow/custom_logging.py
+++ edflow/custom_logging.py
@@ -106,6 +106,7 @@ def fix_abseil():
     # https://github.com/tensorflow/tensorflow/issues/26691#issuecomment-500369493
     try:
         import absl.logging
+
         logging.root.removeHandler(absl.logging._absl_handler)
         absl.logging._warn_preinit_stderr = False
     except Exception:
