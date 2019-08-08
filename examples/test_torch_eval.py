ff836b19daf6e7da299ef53a6906ec28b99c0932
Johannes Haux <jo.mobile.2011@gmail.com>
2019-08-08 11:19:23 +0200

    :tada: :white_check_mark: :memo: Adds commandline interface to edeval for changing config parameters

--- examples/test_torch_eval.py
+++ examples/test_torch_eval.py
@@ -28,5 +28,31 @@ def test_eval():
     )
 
 
+def test_eval_with_additional_kwargs():
+    """Just make sure example runs without errors while adding more kwargs."""
+    output = subprocess.check_output(
+        "edflow -t eval_hook/mnist_config.yaml -n eval_testrun --num_steps 11",
+        shell=True,
+    )
+
+    csv_name = get_model_csv(str(output))
+
+    output = subprocess.run(
+        "edeval -c {} -cb eval_hook.model.empty_callback --batch_size 16".format(
+            csv_name
+        ),
+        shell=True,
+        check=True,
+    )
+
+    output = subprocess.run(
+        "edeval -c {} -cb eval_hook.model.empty_callback --not_in_there TEST".format(
+            csv_name
+        ),
+        shell=True,
+        check=True,
+    )
+
+
 if __name__ == "__main__":
     test_eval()
