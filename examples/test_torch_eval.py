ea74080791bd0c3459e97973b5a0ca3de7be80d9
Johannes Haux <johannes.haux@gmx.de>
2019-08-08 12:23:50 +0200

    Better docstring
    Co-Authored-By: Sandro Braun <6517465+theRealSuperMario@users.noreply.github.com>

--- examples/test_torch_eval.py
+++ examples/test_torch_eval.py
@@ -29,7 +29,14 @@ def test_eval():
 
 
 def test_eval_with_additional_kwargs():
-    """Just make sure example runs without errors while adding more kwargs."""
+    """Make sure example runs without errors while adding more kwargs.
+    
+    1. edflow -t eval_hook/mnist_config.yaml -n eval_testrun --num_steps 11
+    2. edeval -c csv_name -cb eval_hook.model.empty_callback --batch_size 16
+    3. edeval -c csv_name -cb eval_hook.model.empty_callback --not_in_there TEST
+    """
+
+I would like to see all the commands beeing tested in the description of the test
     output = subprocess.check_output(
         "edflow -t eval_hook/mnist_config.yaml -n eval_testrun --num_steps 11",
         shell=True,
