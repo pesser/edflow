1ef617054ebc04d33d0220b29f50267990f8fc47
mritv <mritvik@ymail.com>
2019-08-09 12:33:07 +0200

    The base directory from which the setup should be copied.

---
+++ edflow/edsetup_files/model.py
@@ -0,0 +1,11 @@
+class Model(object):
+    """
+    Clean model skeleton for initialization.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+    def __call__(self, x):
+        return x
