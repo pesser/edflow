fc47092f4272fd3e8913669e8d03a3bcc561e967
Johannes Haux <jo.mobile.2011@gmail.com>
2019-09-12 16:44:51 +0200

    "white_check_mark: :memo: Adds tests and documentation for the cool ProcessedDataset

---
+++ tests/test_data/test_processing/test_processed.py
@@ -0,0 +1,32 @@
+import pytest
+
+from edflow.debug import DebugDataset
+from edflow.data.processing.processed import ProcessedDataset
+
+
+def test_processed_dset():
+    D = DebugDataset(size=10)
+
+    P = ProcessedDataset(D, lambda val, index_, other: {'val': val**2})
+
+    assert len(D) == len(P)
+    assert len(D.labels['label1']) == len(P.labels['label1'])
+
+    dp = P[0]
+    ref = D[0]
+    ref['val'] = ref['val']**2
+
+    assert dp == ref
+
+
+def test_processed_dset_no_update():
+    D = DebugDataset(size=10)
+
+    P = ProcessedDataset(D, lambda val, index_, other: {'val': val**2}, False)
+
+    assert len(D) == len(P)
+    assert len(D.labels['label1']) == len(P.labels['label1'])
+
+    dp = P[0]
+
+    assert dp == {'val': 0, 'index_': 0}
