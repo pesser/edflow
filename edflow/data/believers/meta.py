34f1a448f639cf0386c093abc2873f4f5909ffbb
Johannes Haux <johannes.haux@gmx.de>
2019-12-04 15:40:06 +0100

    Fixes Type

--- edflow/data/believers/meta.py
+++ edflow/data/believers/meta.py
@@ -230,7 +230,7 @@ def load_labels(root):
 
 
 def clean_keys(labels, loaders):
-    """Removes all loader inforamtion from the keys.
+    """Removes all loader information from the keys.
 
     Parameters
     ----------
