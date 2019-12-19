0761b803bc290bb78fb0e391562e7ac9d8872cc4
Patrick Esser <Patrick.Esser@gmx.net>
2019-12-19 14:19:45 +0100

    reduce required dependencies; avoid pytorch incompatible scipy version

--- setup.py
+++ setup.py
@@ -4,6 +4,38 @@ from setuptools import setup, find_packages
 # allows to get version via python setup.py --version
 __version__ = "dev"
 
+install_requires = [
+    "pyyaml",
+    "tqdm",
+    "Pillow",
+    "chainer",
+    "numpy",
+    "pandas",  # for csv dataset and eval pipeline
+    "fastnumbers",  # for dict utils
+]
+
+install_full = [  # for extra functionality
+    "streamlit > 0.49",  # for edexplore
+    "psutil",  # for edlist
+    "scipy<1.4",  # TODO pinned dependency of scikit-image; only until 1.4.1 is out and fixes https://github.com/scipy/scipy/issues/11237
+    "scikit-image",  # for ssim in image_metrics.py
+    "black",  # for formatting of code
+    "matplotlib",  # for plot_datum
+]
+install_docs = [  # for building the documentation
+    "sphinx >= 1.4",
+    "sphinx_rtd_theme",
+    "better-apidoc",
+]
+install_test = install_full + [  # for running the tests
+    "pytest",
+    "pytest-cov",
+    "coveralls",
+    "scipy<1.4",  # TODO pinned dependency of scikit-image; only until 1.4.1 is out and fixes https://github.com/scipy/scipy/issues/11237
+    "scikit-image",  # for some tf tests (also for image_metrics.py but lets avoid heavy dependency for a single functionality)
+]
+extras_require = {"full": install_full, "docs": install_docs, "test": install_test}
+
 
 setup(
     name="edflow",
@@ -15,23 +47,8 @@ setup(
     license="MIT",
     packages=find_packages(),
     package_data={"": ["*.yaml"]},
-    install_requires=[
-        "pyyaml",
-        "tqdm",
-        "Pillow",
-        "chainer",
-        "numpy",
-        "scikit-image",
-        "pandas",
-        "psutil",
-        "deprecated",
-        "fastnumbers",
-    ],
-    extras_require={
-        "explore": ["streamlit > 0.49"],
-        "docs": ["sphinx >= 1.4", "sphinx_rtd_theme", "better-apidoc"],
-        "test": ["pytest", "pytest-cov", "coveralls"],
-    },
+    install_requires=install_requires,
+    extras_require=extras_require,
     zip_safe=False,
     scripts=[
         "edflow/edflow",
